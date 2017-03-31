c2c9944e11538a9bf787f8ffbd3234b0abe7ab05
devdoomari.circleci <devdoomari@gmail.com>
2017-03-31 11:15:53 +0000

    generated from:89493bb0048bf2130b4066e2a70c739e64a12fdd triggered by:devdoomari [ci skip]

---
+++ lib_es6/BaseCustomAttrs.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class BaseCustomAttrs {
+    constructor(attrs) {
+        this.attrs = attrs;
+    }
+}
+exports.BaseCustomAttrs = BaseCustomAttrs;
